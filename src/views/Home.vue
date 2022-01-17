ef14c70f967915ec648948bb768a2f0a4bd961f8
firo <cfiro@qq.com>
2022-01-17 11:22:28 +0800

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,43 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="home">
+    <div class="shell">
+      <audio ref="audio" src="../../public/music/周华健 - 有没有一首歌会让你想起我.mp3"></audio>
+      <button @click="play">播放</button>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  name:"Home",
+  methods: {
+    play(){
+      this.$refs.audio.play()
+    }
+  },
 }
 </script>
+
+<style lang="scss">
+body {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background-color: #efefef;
+}
+#home{
+  background-color: #eee;
+  width: 400px;
+  height: 250px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  .shell {
+    background-color: #fff;
+    width: 80%;
+    height: 80%;
+    border-radius: 40px;
+  }
+}
+</style>
